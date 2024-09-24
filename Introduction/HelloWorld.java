c3954c9123054a253c9eb600e6f779bcbafccd65
MD. Abu Bakar Siddique <140780598+Abs-Futy7@users.noreply.github.com>
2024-09-24 17:53:07 +0600

    Add files via upload

---
+++ Introduction/HelloWorld.java
@@ -0,0 +1,25 @@
+package Introduction;
+class HelloWorld
+{
+	public static void main(String []args) {
+      System.out.println("Hello World"); // prints Hello World
+
+      System.out.print("One");
+      System.out.print("Two");
+      System.out.println("Three");  // println print the output and adds a newline 
+
+
+      System.out.println("One");    // print prints the output only, not adding new linev
+      System.out.println("Two");
+      System.out.println("Three");
+
+      System.out.println("");
+
+      System.out.println("*");
+      System.out.println("**");
+      System.out.println("***");
+      System.out.println("****");
+
+      
+   }
+}
