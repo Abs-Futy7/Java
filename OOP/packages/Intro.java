dd118107161f65451f0d1fddfdc560eddaac2336
MD. Abu Bakar Siddique <140780598+Abs-Futy7@users.noreply.github.com>
2024-10-03 21:46:16 +0600

    Add files via upload

---
+++ OOP/packages/Intro.java
@@ -0,0 +1,10 @@
+package packages;
+
+import static packages.Message.message;
+
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        message(); // Calls the message method from Message class
+    }
+}
---
+++ OOP/packages/Message.java
@@ -0,0 +1,12 @@
+package packages;
+
+public class Message {
+    public static void main(String[] args) {
+        // You can add something here if needed, or remove the main method if unnecessary
+    }
+
+    // Correctly defined message method
+    public static void message() { // have to declare it in public to acces it in other files
+        System.out.println("This is message");
+    }
+}
